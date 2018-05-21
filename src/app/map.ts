c76c572a665c1f2b04e2f985a53ed0d2c2718ba8
amalfushi <amalfushi@gmail.com>
2018-05-21 00:09:28 -0700

    Added tile generation and printing the map;

--- src/app/map.ts
+++ src/app/map.ts
@@ -1,2 +1,36 @@
 export class Map {
+
+    tiles: number[][];
+    columns: number;
+    rows: number;
+
+    constructor(width: number, height: number) {
+        this.rows = height;
+        this.columns = width;
+        this.generateTiles(width, height);
+    }
+
+    generateTiles(width: number, height: number): Map {
+        this.tiles = [];
+        let i = 0;
+        for (let y = 0; y < height; y++) {
+            let row = [];
+            for (let x = 0; x < width; x++) {
+                row.push(i++);
+            }
+            this.tiles.push(row);
+        }
+
+        return this;
+    }
+
+    print(): void {
+        console.log(`${this.columns}x${this.rows}`)
+        for (let row of this.tiles) {
+            console.log(row)
+        }
+    }
+
+    getColumns(): number { return this.columns }
+    getRows(): number { return this.rows }
 }
