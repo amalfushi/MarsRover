b649a47c2b9af395c456ee67a45d93830e5f6a37
amalfushi <amalfushi@gmail.com>
2018-05-21 18:37:36 -0700

    adjusted for new rover and map services

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,72 +2,94 @@ import { Component, OnInit } from '@angular/core';
 import { Rover } from '../rover';
 import { Map } from '../map'
 import { Facing } from '../direction.enum';
+import { RoverService } from '../rover.service';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
+  providers: [RoverService, MapService]
 })
 export class GameComponent implements OnInit {
 
   rovers: Rover[];
-  start: string;
-  newMapSize: string;
+  start: string = "";
+  newMapSize: string = "";
   map: Map;
 
-  constructor() {
-    this.rovers = [];
-    this.start = "";
-    this.newMapSize = "";
+  constructor(private roverService: RoverService, private mapService: MapService) {
   }
 
   ngOnInit() {
+    this.getRovers();
+  }
+
+
+
+  resetAll(): void {
+    this.rovers = [];
+    this.map = null;
+  }
+
+
+  // Map Manipulation
+  getMap():void {
+    this.map = this.mapService.getMap();
   }
 
   createMap(): void {
     const mapSpecs = this.parseCoordinates(this.newMapSize, /^[2-9][2-9]$/);
     if (mapSpecs) {
-      this.map = new Map(mapSpecs[0], mapSpecs[1])
-      this.newMapSize = "";
+      this.mapService.generateTiles(mapSpecs[0], mapSpecs[1]);
+      this.getMap();
     }
-    // this.map.print();
+  }
+
+
+  // Rover Manipulation
+  getRovers() {
+    this.rovers = this.roverService.getRovers();
   }
 
   addRover(): void {
     const coords = this.parseCoordinates(this.start, /^\d{2}[NESW]$/);
     if (coords && coords.length === 3) {
-      // change the rover direction from a character to a proper Facing
-      coords[2] = coords[2] == "N" ? Facing.north : coords[2] == "E" ? Facing.east : coords[2] == "S" ? Facing.south : Facing.west;
-      const rover = new Rover(coords[0], coords[1], coords[2]);
-      this.rovers.push(rover);
+      const rover = this.roverService.addRover(coords[0], coords[1], coords[2]);
+      this.mapService.addRover(rover)
+      this.getRovers();
+      this.getMap();
       this.start = "";
     }
   }
 
+  deleteRover(rover: Rover): void {
+    this.roverService.deleteRoverById(rover.id);
+    this.getRovers();
+  }
+
+  executeInstructions(): void {
+    for (let rover of this.rovers) {
+      if (rover.instructions && rover.status) {
+        // to replace rove on map
+        // const start = [rover.row, rover.column];
+        this.mapService.removeRover(rover);
+        rover.instructions = this.removeSpaces(rover.instructions);
+        this.roverService.executeInstructionsById(rover.id, this.map.columns, this.map.rows);
+        this.mapService.addRover(rover);
+        this.getMap();
+      }
+    }
+  }
+
+
+  // String parsing
   parseCoordinates(str: string, reg: RegExp): any {
     const noSpaces = this.removeSpaces(str);
     if (noSpaces.match(reg)) {
       return this.toIntegers(this.removeSpaces(noSpaces).split(""));
     }
     return null;
-  }
-
-  executeInstructions(): void {
-    this.rovers.forEach((rover) => {
-
-      // console.log(rover.instructions)
-      if (rover.instructions) {
-        const instArray = this.removeSpaces(rover.instructions).split("");
-        // console.log(instArray)
-        instArray.forEach((instruction) => {
-          if (rover.status) {
-            rover.executeInstruction(instruction);
-            rover.checkForBoundaries(this.map.columns + 1, this.map.rows + 1)
-          }
-        });
-        rover.instructions = "";
-      }
-    });
   }
 
   removeSpaces(str: string): string {
